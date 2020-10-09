6480a92caabc41f2687bc619c473ebc95b9c9cc9
andrewta999 <andrewta365@gmail.com>
2020-10-09 16:34:12 -0400

    added jest testing framework

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (_, res) => {
-    res.send('hot reload is enabled');
+    res.send('Main Page');
 });
 
 app.listen(port, () => {
-    return console.log(`server is listening on ${port}`);
+    return console.log(`Server is listening on ${port}`);
 });
